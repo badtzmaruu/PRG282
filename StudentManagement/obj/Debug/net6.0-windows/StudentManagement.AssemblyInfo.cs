bdbece28a1f996c424c2fd1e880b92694187f43f
KeenanLombard <keenanlombardd2@gmail.com>
2024-11-10 22:53:03 +0200

    frontend changes

--- StudentManagement/obj/Debug/net6.0-windows/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net6.0-windows/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e95259cfceadf6d4a66a556408a30ff5ad86136")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
