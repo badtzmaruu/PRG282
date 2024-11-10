2e95259cfceadf6d4a66a556408a30ff5ad86136
Jonathan Joubert <rjoubert@fibco.co.za>
2024-11-10 22:29:03 +0200

    new project, removed duplicate code

--- StudentManagement/obj/Debug/net6.0-windows/StudentManagement.AssemblyInfo.cs
+++ StudentManagement/obj/Debug/net6.0-windows/StudentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dba326e47714174de56caa68f90fc225708eb386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b07e6ef17c7a66d00f4206d81538a93a48a259c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
